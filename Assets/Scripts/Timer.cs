87c2d32ffb6ddf61cf7ed3ec95e2be5858945d1b
emyrysf <bolhocaliogluemiryusuf@gmail.com>
2024-08-19 19:39:46 +0300

    (Add) Task Manager

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Timer 
+{
+    public float time;
+    private float targetTime;
+
+    public bool TimeOut
+    {
+        get { return targetTime < Time.time ; }
+    }
+
+    public Timer(float time)
+    {
+        this.time = time;
+    }
+
+    public void StartTimer()
+    {
+        targetTime = Time.time;
+    }
+
+    
+}
